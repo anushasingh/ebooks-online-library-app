6804e06e14c4c0494a8cec917aa8d75084c07e09
ANUSHA-DELL\Anusha <anushasingh996@gmail.com>
2017-09-27 13:09:13 +0530

    search functionality added

--- src/app/datafilter.pipe.ts
+++ src/app/datafilter.pipe.ts
@@ -4,11 +4,33 @@ import * as _ from "lodash";
   name: 'datafilter'
 })
 export class DatafilterPipe implements PipeTransform {
-
+ arr=[];
     transform(array: any[], query: string): any {
+       // console.log(query);
+// for(let i=0;i<array.length;i++)
+// {
+//     this.arr.push(array[i]);
+// }
+
+let name;
+let author;
+
         if (query) {
-            return _.filter(array, row=>{ row.name.indexOf(query) > -1});
-        }
+
+            return _.filter(array, row=>{
+                   name= row.name.indexOf(query) > -1;
+                   author= row.author.indexOf(query)> -1;
+               // ( row.name.indexOf(query) > -1)&& ( row.author.indexOf(query)> -1)
+               return name||author;
+                 
+            }
+                 ); 
+                
+            
+              }
+        
+      
+
         return array;
     }
 }
